4d24f507a70995b299cfc662093ef11d94b94ecc
KirbyKingston <31425924+KirbyKingston@users.noreply.github.com>
2018-03-30 12:01:55 -0500

    Add files via upload

---
+++ FirstWebApp/src/edu/wctc/dj/first/FirstServlet.java
@@ -0,0 +1,23 @@
+package edu.wctc.dj.first;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String html = "<html><body>Hi. I received param1=" +
+                request.getParameter("param1") +
+                " via POST</body><html>";
+        response.getOutputStream().println(html);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String html = "<html><body>Hi. I received param1=" +
+                request.getParameter("param1") +
+                " via GET</body><html>";
+        response.getOutputStream().println(html);
+    }
+}
